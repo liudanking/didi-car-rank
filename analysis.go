f738fa30163413d0a487f529d420888a64077680
liudanking <liudanking@gmail.com>
2018-06-20 22:41:41 +0800

    update analysis

--- analysis.go
+++ analysis.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"sort"
+
+	"github.com/olekukonko/tablewriter"
 
 	"github.com/liudanking/goutil/encodingutil"
 	log "github.com/liudanking/goutil/logutil"
@@ -15,6 +18,7 @@ func analysisCity(c *cli.Context) error {
 	city := c.String("city")
 	analylizer := NewCityAnalyzer(dir, city)
 	analylizer.analysisCurrentOrder()
+	analylizer.analysisRepurchase()
 	return nil
 }
 
@@ -76,8 +80,75 @@ func (ca *CityAnalyzer) analysisCurrentOrder() {
 	sort.Slice(carModelCountList, func(i, j int) bool { return carModelCountList[i].Count > carModelCountList[j].Count })
 
 	log.Notice("car rank:")
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"#", "车型", "实时订单数量"})
 	for i, mc := range carModelCountList {
-		log.Info("#%d\t%s\t%d", i, mc.Model, mc.Count)
+		if i > 20 {
+			break
+		}
+		table.Append([]string{
+			fmt.Sprint(i),
+			mc.Model,
+			fmt.Sprint(mc.Count),
+		})
 	}
+
+	table.Render()
+
+}
+
+type CarModelScore struct {
+	Model string
+	Score int
+}
+
+func (ca *CityAnalyzer) analysisRepurchase() {
+	repurchaseDir := filepath.Join(ca.cityDataDir, "repurchase")
+
+	modelScore := map[string]int{}
+	filepath.Walk(repurchaseDir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		items := map[string]RepurchaseItem{}
+		if err := encodingutil.UnmarshalJSONFromFile(path, &items); err != nil {
+			log.Warning("unmarshal from %s failed:%v", path, err)
+			return nil
+		}
+
+		for _, item := range items {
+			if item.CarModel != "" {
+				modelScore[item.CarModel] += item.OrderCount1M
+			}
+		}
+
+		return nil
+	})
+
+	carModelScoreList := []CarModelScore{}
+	for model, score := range modelScore {
+		carModelScoreList = append(carModelScoreList, CarModelScore{
+			Model: model,
+			Score: score,
+		})
+	}
+
+	sort.Slice(carModelScoreList, func(i, j int) bool { return carModelScoreList[i].Score > carModelScoreList[j].Score })
+
+	log.Notice("car score rank:")
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"#", "车型", "加油积分"})
+	for i, ms := range carModelScoreList {
+		if i > 20 {
+			break
+		}
+		table.Append([]string{
+			fmt.Sprint(i),
+			ms.Model,
+			fmt.Sprint(ms.Score),
+		})
+	}
+	table.Render()
 
 }
